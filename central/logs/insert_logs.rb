9a04774ac9850e0543856bb47971b969ff06b622
root <root@example.com>
2024-10-23 12:10:51 +0200

    log slow query et cpu usage eleve

--- central/logs/insert_logs.rb
+++ central/logs/insert_logs.rb
@@ -15,7 +15,7 @@ client.query("USE logs_db;")
 
 # Création des tables pour les logs si elles n'existent pas déjà
 client.query <<-SQL
-  CREATE TABLE IF NOT EXISTS system_logs (
+  CREATE TABLE IF NOT EXISTS wordpress_system_logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     log_time DATETIME,
     log_message TEXT,
@@ -39,8 +39,9 @@ client.query <<-SQL
   );
 SQL
 
+# Table pour les logs système de MariaDB
 client.query <<-SQL
-  CREATE TABLE IF NOT EXISTS mariadb_logs (
+  CREATE TABLE IF NOT EXISTS mariadb_system_logs (
     id INT AUTO_INCREMENT PRIMARY KEY,
     log_time DATETIME,
     log_message TEXT,
@@ -48,12 +49,23 @@ client.query <<-SQL
   );
 SQL
 
-# Méthode pour insérer un log système
-def insert_system_log(client, log_time, log_message)
+
+
+client.query <<-SQL
+  CREATE TABLE IF NOT EXISTS mariadb_error_logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    log_time DATETIME,
+    log_message TEXT,
+    UNIQUE(log_time, log_message) -- Pour éviter les doublons
+  );
+SQL
+
+# Méthode pour insérer un log système WordPress
+def insert_wordpress_system_log(client, log_time, log_message)
   begin
-    client.prepare("INSERT IGNORE INTO system_logs (log_time, log_message) VALUES (?, ?)").execute(log_time, log_message)
+    client.prepare("INSERT IGNORE INTO wordpress_system_logs (log_time, log_message) VALUES (?, ?)").execute(log_time, log_message)
   rescue Mysql2::Error => e
-    puts "Erreur lors de l'insertion du log système: #{e.message}"
+    puts "Erreur lors de l'insertion du log système WordPress: #{e.message}"
   end
 end
 
@@ -82,15 +94,24 @@ def insert_apache_log(client, log_line)
   end
 end
 
+# Méthode pour insérer un log système MariaDB
+def insert_mariadb_system_log(client, log_time, log_message)
+  begin
+    client.prepare("INSERT IGNORE INTO mariadb_system_logs (log_time, log_message) VALUES (?, ?)").execute(log_time, log_message)
+  rescue Mysql2::Error => e
+    puts "Erreur lors de l'insertion du log système MariaDB: #{e.message}"
+  end
+end
+
 # Méthode pour insérer un log MariaDB
-def insert_mariadb_log(client, log_line)
+def insert_mariadb_error_log(client, log_line)
   regex = /(?<log_time>\d{4}-\d{2}-\d{2}\s+\d{1,2}:\d{2}:\d{2})\s+\d+\s+\[(?<log_message>[^\]]+\].*)/
   match = regex.match(log_line)
 
   if match
     log_time = DateTime.strptime(match[:log_time], "%Y-%m-%d %H:%M:%S")
     begin
-      client.prepare("INSERT IGNORE INTO mariadb_logs (log_time, log_message) VALUES (?, ?)").execute(log_time, match[:log_message])
+      client.prepare("INSERT IGNORE INTO mariadb_error_logs (log_time, log_message) VALUES (?, ?)").execute(log_time, match[:log_message])
     rescue Mysql2::Error => e
       puts "Erreur lors de l'insertion du log MariaDB: #{e.message}"
     end
@@ -99,31 +120,46 @@ def insert_mariadb_log(client, log_line)
   end
 end
 
+
+
+
 # Traitement des fichiers de log
 def process_logs(client)
   # Chemins des fichiers de log
-  system_log_path = './wordpress/system/syslog'
+  wordpress_system_log_path = './wordpress/system/syslog'
   apache_access_log_path = './wordpress/apache/wordpress_access.log'
-  mariadb_log_path = './sgbd/mariadb/error.log'
+  mariadb_system_log_path = './sgbd/system/syslog'
+  mariadb_error_log_path = './sgbd/mariadb/error.log'
 
-  # Traitement des logs système
-  File.foreach(system_log_path) do |line|
-    log_time = DateTime.parse(line.split[0..1].join(' '))
-    insert_system_log(client, log_time, line.strip)
-  end
+  # Traitement des logs système WordPress
+  # Traitement des logs système WordPress
+File.foreach(wordpress_system_log_path, encoding: 'UTF-8') do |line|
+  log_time = DateTime.parse(line.split[0..1].join(' '))
+  insert_wordpress_system_log(client, log_time, line.strip)
+end
+
 
   # Traitement des logs Apache
   File.foreach(apache_access_log_path) do |line|
     insert_apache_log(client, line.strip)
   end
 
-  # Traitement des logs MariaDB
-  File.foreach(mariadb_log_path) do |line|
-    insert_mariadb_log(client, line.strip)
+  # Traitement des logs système MariaDB
+  File.foreach(mariadb_system_log_path, encoding: 'UTF-8') do |line|
+    log_time = DateTime.parse(line.split[0..1].join(' '))
+    insert_mariadb_system_log(client, log_time, line.strip)
   end
+
+  # Traitement des logs MariaDB
+  File.foreach(mariadb_error_log_path) do |line|
+    insert_mariadb_error_log(client, line.strip)
+  end
+  
+
 end
 
 # Exécution du traitement des logs
 process_logs(client)
 
-puts "Logs inserted successfully."
+puts "Logs insérés avec succès."
+
