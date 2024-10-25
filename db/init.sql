ac4d0b2ac5550260f8d3fba3b59ec5a7cbcd9411
lucas vanneste <luskay59@example.com>
2024-10-25 11:26:23 +0200

    vfinal

--- db/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS logs_db;
-
--- Vérifiez si l'utilisateur existe, sinon créez-le
-CREATE USER IF NOT EXISTS 'user'@'%' IDENTIFIED BY 'password';
-
-GRANT ALL PRIVILEGES ON logs_db.* TO 'user'@'%';
-
-FLUSH PRIVILEGES;
-
--- db_data/init.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE logs_db;
-GRANT ALL PRIVILEGES ON logs_db.* TO 'user'@'%';
-FLUSH PRIVILEGES;
