314313160272822455968a10a1e058ccde9d46f4
lucas vanneste <luskay59@example.com>
2024-11-05 11:54:36 +0100

    changement permission

--- central/logs/execute_log_collection.rb
+++ central/logs/execute_log_collection.rb
@@ -6,14 +6,14 @@ require 'net/scp'
 machines = [
   {
     host: '172.150.0.3',
-    user: 'root',
-    password: 'testpsswd',
+    user: 'limiteduser',
+    password: 'testpassword',
     script: '/var/lib/mysql/script/log_collector_mariadb.rb'
   },
   {
     host: '172.150.0.2',
-    user: 'root',
-    password: 'testpsswd',
+    user: 'limiteduser',
+    password: 'testpassword',
     script: '/var/www/html/script/log_collector_wordpress.rb'
   }
 ]
@@ -23,7 +23,7 @@ def execute_remote_script(machine)
   puts "Connexion à #{machine[:host]}..."
   Net::SSH.start(machine[:host], machine[:user], password: machine[:password], verify_host_key: :never) do |ssh|
     puts "Exécution du script #{machine[:script]}..."
-    command = "ruby #{machine[:script]} 172.150.0.5 root testpsswd" # Remplacez par les bons paramètres
+    command = "ruby #{machine[:script]} 172.150.0.5 user user123" # Remplacez par les bons paramètres
     output = ssh.exec!(command)
     puts output
   end
